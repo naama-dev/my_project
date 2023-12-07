d1bb43e7098882992e72788c9e97b04334cf617c
naama-dev <n0527192989@gmail.com>
2023-12-07 20:40:26 +0200

    project upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import {Routes,Route,Link} from 'react-router-dom'
+import HomeComp from './Home/HomeComp';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" dir="rtl">
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" >
+        <Toolbar>
+          <Link
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Home   
+          </Link>
+          <Link
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            ToDo   
+          </Link>
+          <Link
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Post   
+          </Link> 
+          <Link
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Photo    
+          </Link>
+          <Link
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Users    
+          </Link>
+        </Toolbar>
+      </AppBar>
+      </Box>
+      <Routes>
+        <Route path="/" element={<HomeComp/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Home/HomeComp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+const HomeComp=()=>{
+    return(
+        <>hello</>
+    )
+}
+export default HomeComp
