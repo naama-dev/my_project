299377904b01bfa3c6e5b8f67b59ecf5b2f930e6
naama-dev <n0527192989@gmail.com>
2023-12-10 13:34:51 +0200

    home

---
+++ src/Component/Home/HomeComp.jsx
@@ -0,0 +1,12 @@
+import { Dialog } from "@mui/material";
+import React from "react";
+
+const HomeComp=()=>{
+    return(
+        <> 
+        hello
+      
+        </>
+    )
+}
+export default HomeComp
